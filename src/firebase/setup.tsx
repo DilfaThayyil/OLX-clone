e6881b0ec73b93528ca0beb365fdf8ff339e5ef6
DilfaThayyil <dilfathayyil@gmail.com>
2024-09-03 19:48:23 +0530

    firstCommit

---
+++ src/firebase/setup.tsx
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+import { getAuth,GoogleAuthProvider } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDihJ1nO7ItAIsyhBzt3IfMv3Dn87rhRiw",
+  authDomain: "olx-clone-66548.firebaseapp.com",
+  projectId: "olx-clone-66548",
+  storageBucket: "olx-clone-66548.appspot.com",
+  messagingSenderId: "851986154706",
+  appId: "1:851986154706:web:31f2a72ce9b50be837d90b"
+};
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app)
+export const googleProvider = new GoogleAuthProvider()
