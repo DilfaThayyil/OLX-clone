3846573128f0a97257a3aa546da1058bf2cec738
DilfaThayyil <dilfathayyil@gmail.com>
2024-09-18 14:39:50 +0530

    sellProduct modified with some bugs

--- src/components/sellProduct.tsx
+++ src/components/sellProduct.tsx
@@ -1,21 +1,51 @@
 import { useState } from "react"
+import {ref,uploadBytes,getDownloadURL} from "firebase/storage"
+import { storage } from "../firebase/setup"
+import axios from "axios"
 
 const SellProduct = ()=>{
     const [title,setTitle] = useState('')
     const [price,setPrice] = useState('')
     const [description,setDescription] = useState('')
     const [category,setCategory] = useState('')
-    const [image,setImage] = useState<File[]>([])
+    const [images,setImage] = useState<File[]>([])
+    const [uploading,setUploading] = useState(false)
 
     const handleImageUpload = (e:any)=>{
         setImage(e.target.files)
     }
 
-    const handleSubmit = async(e:any)=>{
-        e.preventDefault()
+    const uploadImagesToFirebase = ()=>{
+
+        const uploadedUrls:string[] = []
+
+        for (let i = 0; i < images.length; i++) {
+            const image = images[i];
+            const storageRef = ref(storage, `products/${image.name}`);
+            const snapshot = await uploadBytes(storageRef, image);
+            const downloadURL = await getDownloadURL(snapshot.ref);
+            uploadedUrls.push(downloadURL);
+        }
+        return uploadedUrls
     }
 
-    
+    const handleSubmit = async(e:any)=>{
+        e.preventDefault()
+        setUploading(true)
+    }
+
+    try{
+        const uploadedImageUrls = await uploadImagesToFirebase()
+        const productData = {
+            title,
+            price,
+            description,
+            category,
+            images:uploadedImageUrls
+        }
+        await axios.post("",productData)
+        
+    }
 
     return (
         <form onSubmit={handleSubmit} className="p-4">
