37b44f7e13c7deb73e1204f71036fd676d2cbe16
DilfaThayyil <dilfathayyil@gmail.com>
2024-09-18 14:12:28 +0530

    added firebase storage setup

--- src/firebase/setup.tsx
+++ src/firebase/setup.tsx
@@ -1,5 +1,6 @@
 import { initializeApp } from "firebase/app";
-import { getAuth,GoogleAuthProvider } from "firebase/auth";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
+import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
   apiKey: "AIzaSyDihJ1nO7ItAIsyhBzt3IfMv3Dn87rhRiw",
@@ -10,7 +11,7 @@ const firebaseConfig = {
   appId: "1:851986154706:web:31f2a72ce9b50be837d90b"
 };
 
-
 const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app)
-export const googleProvider = new GoogleAuthProvider()
+export const auth = getAuth(app);
+export const googleProvider = new GoogleAuthProvider();
+export const storage = getStorage(app); // Firebase Storage setup
