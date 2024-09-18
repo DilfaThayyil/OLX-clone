5cad57dba2a95d6e273f103ef4fac8d99d2683c9
DilfaThayyil <dilfathayyil@gmail.com>
2024-09-18 14:13:15 +0530

    newRoute SellProduct added

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { Route, Routes } from "react-router-dom"
 import Main from "./components/Main"
 import Details from "./components/Details"
+import SellProduct from "./components/sellProduct"
 
 const App = () => {
   return (
@@ -8,6 +9,7 @@ const App = () => {
    <Routes>
    <Route path="/" element={<Main/>}/>
    <Route path="/details" element={<Details/>}/>
+   <Route path="/sellProduct" element={<SellProduct/>}/>
    </Routes>
    </>
   )
--- src/firebase/setup.tsx
+++ src/firebase/setup.tsx
@@ -14,4 +14,4 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const googleProvider = new GoogleAuthProvider();
-export const storage = getStorage(app); // Firebase Storage setup
+export const storage = getStorage(app); 
