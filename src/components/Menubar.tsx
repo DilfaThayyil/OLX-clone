788d24b511ffe4f4f74dd6b17a7c0cf6dd85b348
DilfaThayyil <dilfathayyil@gmail.com>
2024-09-20 11:38:48 +0530

    menubar modified

--- src/components/Menubar.tsx
+++ src/components/Menubar.tsx
@@ -6,13 +6,10 @@ type menuProp = {
   const Menubar = (props:menuProp) => {
     return (
       <div className="flex shadow-sm h-10 p-2">
-        <h1 onClick={()=> props?.setMenu("Shirt")} className="ml-48 cursor-pointer">Shirt</h1>
-        <h1 onClick={()=> props?.setMenu("Jacket")} className="ml-5 cursor-pointer">Jacket</h1>
-        <h1 onClick={()=> props?.setMenu("phones")} className="ml-5 cursor-pointer">Mobile phones</h1>
-        <h1 onClick={()=> props?.setMenu("House")} className="ml-5 cursor-pointer">House</h1>
-        <h1 onClick={()=> props?.setMenu("Scooters")} className="ml-5 cursor-pointer">Scooters</h1>
-        <h1 onClick={()=> props?.setMenu("Bike")} className="ml-5 cursor-pointer">Bike</h1>
-        <h1 onClick={()=> props?.setMenu("Apartments")} className="ml-5 cursor-pointer">Apartments</h1>
+        <h1 onClick={()=> props?.setMenu("Beauty")} className="ml-48 cursor-pointer">Beauty</h1>
+        <h1 onClick={()=> props?.setMenu("Fragrances")} className="ml-5 cursor-pointer">Fragrances</h1>
+        <h1 onClick={()=> props?.setMenu("Furniture")} className="ml-5 cursor-pointer">Furniture</h1>
+        <h1 onClick={()=> props?.setMenu("Groceries")} className="ml-5 cursor-pointer">Groceries</h1>
       </div>
     )
   }
